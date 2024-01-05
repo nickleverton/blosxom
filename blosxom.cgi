nickleverton/blosxom

commit 1d9899f2726e343ccc813414d0d369e7360638ba

Simplify default entries sub, removing hanging $1/$2 refs.

blosxom.cgi
@@ -557,16 +557,11 @@ $entries = sub {
             return if $depth and $curr_depth > $depth;
             return if !-r $File::Find::name;
 
-            if (
-
-                # a match
-                $File::Find::name
-                =~ m!^$datadir/(?:(.*)/)?(.+)\.$file_extension$!
-
-                # not an index, .file, and is readable
-                and $2 ne 'index' and $2 !~ /^\./
-                )
+            # if a $file_extension file and not a .file or an index
+            if ( m/^([^.].*)\.$file_extension$/
+                and $1 ne 'index' )
             {
+                my $basename_noext = $1;
 
                 # read modification time
                 my $mtime = stat($File::Find::name)->mtime or return;
@@ -578,16 +573,17 @@ $entries = sub {
                 $files{$File::Find::name} = $mtime;
 
                 # static rendering bits
+                (my $dirname = $File::Find::dir) =~ s!^$datadir/?!!;
                 my $static_file
-                    = "$static_dir/$1/index." . $static_flavours[0];
+                    = "$static_dir/${dirname}index.$static_flavours[0]";
                 if (   $param_all
                     or !-f $static_file
                     or stat($static_file)->mtime < $mtime )
                 {
-                    $indexes{$1} = 1;
+                    $indexes{$dirname} = 1;
                     my $d = join( '/', ( nice_date($mtime) )[ 5, 2, 3 ] );
                     $indexes{$d} = $d;
-                    $indexes{ ( $1 ? "$1/" : '' ) . "$2.$file_extension" } = 1
+                    $indexes{"$dirname$basename_noext.$file_extension"} = 1
                         if $static_entries;
                 }
             }

commit ca97975d66a1cde50c04fc35e7fec19e639e237f

Fix static rendering with entries subroutine
When we have "article.txt" in a directory named "mytheme", we got a file
"mythemearticle.html" generated at the top level of static directory.
What is expected is a file "article.html" generated in the directory
"mytheme" which itself is contained by the static directory.

The bug was introduced by commit 1d9899f : a test done on $1 being empty
or not was not transposed.
I became aware of the bug only today, as I routinely use the
"entries_cache" plugin as a substitute of the default "entries"
subroutine.

blosxom.cgi
@@ -608,7 +608,7 @@ $entries = sub {
                             my $d = join( '/', ( nice_date($mtime) )[ 5, 2, 3 ] );
                             $indexes{$d} = $d;
                         }
-                        $indexes{"$dirname$basename_noext.$file_extension"}
+                        $indexes{ ($dirname ? "$dirname/" : '') . "$basename_noext.$file_extension" }
                             = 1
                             if $static_entries;
                     }